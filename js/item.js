6668e44414d67158100607617f1f42a63f2c54ef
Alejandra Gonzalez <alegonzalez24@hotmail.com>
2023-09-23 10:55:21 -0300

    Js item

--- js/item.js
+++ js/item.js
@@ -1,2 +1,8 @@
 const add=document.querySelector("#add");
 const substract=document.querySelector("#substract");
+const quantity= document.querySelector("#quantity");
+add.addEventListener('click', ()=> quantity.value=Number(quantity.value) +1);
+substract.addEventListener('click', ()=> {
+  if (quantity.value>=1) {
+    quantity.value=Number(quantity.value) -1 }});
+;
